c84ee1ca789de10f075a7b39bc77df9d0adc72a9
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-02-08 17:47:23 +0100

    create Navbar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Outlet } from 'react-router';
 import './App.css'
+import Navbar from './components/Navbar';
 
 function App() {
 
 
   return (
     <>
-      <nav>Navbar</nav>
-      <main>
+      <Navbar/>
+      <main className='max-w-7xl mx-auto min-h-screen mt-12'>
         <Outlet/>
       </main>
     </>
