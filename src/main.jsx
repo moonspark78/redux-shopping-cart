a103c8bde68fce8681d4f4a35c74a04d3a2a044a
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-02-12 19:16:45 +0100

    create the Product  component

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import Home from "./pages/home/Home.jsx";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
       <Routes>
         <Route element={<App />}>
-          <Route path="/" element={<div>Home Page</div>} />
+          <Route path="/" element={<Home/>} />
           <Route path="/products" element={<div>Products Page</div>} />
           <Route path="/cart" element={<div>Cart Page</div>} />
         </Route>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 const Home = () => {
+
+    const products = useSelector((state) => state.products)
+    console.log(products);
+    
+
+
+
   return (
-    <div>Home</div>
+    <div className='py-8 max-w-7xl mx-auto px-4'>
+        <div className='grid lg:grid-cols-3 grid-cols-1 gap-8'>
+            <div className='col-span-2'>Display Products</div>
+            <div>Add New Products</div>
+        </div>
+    </div>
   )
 }
 
