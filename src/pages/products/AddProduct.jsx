e0609b808a81f7547449ffebb49617f324cce7eb
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-02-15 13:49:16 +0100

    working in AddProduct create the select  :art:

--- src/pages/products/AddProduct.jsx
+++ src/pages/products/AddProduct.jsx
@@ -5,12 +5,28 @@ const AddProduct = () => {
     <div className='max-w-md mx-auto bg-white shadow-lg rounded-lg p-6'>
         <h2 className='text-sl font-semibold text-gray-700 mb-4'>Add New Product</h2>
 
-        <form>
+        <form className='space-y-4'>
+
+            {/* Name */}
             <div>
                 <label className='block text-sm font-medium text-gray-600'>Product Name : </label>
                 <input type='text' name='name' id='name' placeholder='Product 1' className='w-full bg-gray-50 text-black mt-1 
                 p-2 rounded-lg border focus:outline-none focus:ring-2 focus:ring-indigo-600'/>
             </div>
+
+            {/* Category */}
+            <div>
+                <label className='block text-sm font-medium text-gray-600'>Category Name : </label>
+                <select name='category' id='category' className='w-full bg-gray-50 text-black mt-1 
+                p-2 rounded-lg border focus:outline-none focus:ring-2 focus:ring-indigo-600'>
+                    <option value=''>Select a Category</option>
+                    <option value='clothing'>Clothing</option>
+                    <option value='gadgets'>Gadgets</option>
+                    <option value='beauty'>Beauty</option>
+                </select>
+            </div>
+
+
         </form>
     </div>
   )
