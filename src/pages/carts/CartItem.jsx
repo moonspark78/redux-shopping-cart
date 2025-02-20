1cff9f1ee124a8671cd615b18d9e75f25aa1909f
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-02-20 18:18:26 +0100

    :wrench: working  CartItem    :art:

--- src/pages/carts/CartItem.jsx
+++ src/pages/carts/CartItem.jsx
@@ -8,6 +8,15 @@ const CartItem = ({item}) => {
             <div className='sm:flex sm:justify-start shadow-md p-6 bg-white rounded-lg mb-6 justify-between'>
                 {/* product image */}
                 <img src={image} alt='' className='w-full h-28 sm:w-40 object-cover rounded'/>
+
+                <div className='sm:ml-4 sm:flex sm:w-full sm:justify-between'>
+
+                    {/* product details */}
+                    <div>
+                        <h3 className='text-lg font-bold text-gray-900'>{name}</h3>
+                        <p className='text-sm text-gray-500'>{price}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
