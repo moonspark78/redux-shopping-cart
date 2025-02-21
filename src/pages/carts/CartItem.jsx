906897a01b2578cfd03f57370e5ba8a2bc97b0e7
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-02-21 16:28:32 +0100

    :wrench: working in  CartItem  :art:

--- src/pages/carts/CartItem.jsx
+++ src/pages/carts/CartItem.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { ImCross } from "react-icons/im";
+
 
 const CartItem = ({item}) => {
     const {id, name, price, image, category, quantity} = item || {};
@@ -19,8 +21,8 @@ const CartItem = ({item}) => {
                     </div>
 
                     {/*  */}
-                    <div>
-                        <div>
+                    <div className='mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6'>
+                        <div className='flex items-center border-gray-100'>
                             <span className='cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100
                             hover:bg-blue-500 hover:text-white'>-</span>
                             <input type='number' value={quantity} className='size-8 border bg-white text-center
@@ -28,7 +30,16 @@ const CartItem = ({item}) => {
                             <span className='cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100
                             hover:bg-blue-500 hover:text-white'>+</span>
                         </div>
+
+                        <div className='flex items-center space-x-4'>
+                            <p>${(price*quantity).toFixed(2)}</p>
+                            <button className="text-red-500 cursor-pointer active:opacity-90" >
+                                <ImCross />
+                            </button>
+                        </div>
                     </div>
+
+                    
 
 
                 </div>
