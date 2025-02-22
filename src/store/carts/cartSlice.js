6a2309fb9a859762e4b57535bf3297179fc7fead
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-02-22 14:47:24 +0100

    :wrench: in cartSlice create the decreaseQuantity reducer  :art:

--- src/store/carts/cartSlice.js
+++ src/store/carts/cartSlice.js
@@ -22,10 +22,16 @@ const cartSlice = createSlice({
             if (product) {
                 product.quantity++
             }
-        }
-
+        },
 
         //decrease quantity
+        decreaseQuantity: (state, action) => {
+            const product = state.find((product) => product.id === action.payload)
+            if (product && product.quantity > 1) {
+                product.quantity--
+            }
+        },
+
         //remove from Cart
     },
 })
